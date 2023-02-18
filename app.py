1447e56998743ae62fce47325142aafeaeaa3399
comfyanonymous <comfyanonymous@protonmail.com>
2023-02-18 01:49:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ mimetypes.add_type('application/javascript', '.js')
 
 from flask import Flask
 
-app = Flask(__name__, static_folder='./static', static_url_path='')
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
 def index():
-    return app.send_static_file('html/index.html')
+    return app.send_static_file('index.html')
 
 app.run(port=55502, debug=True)
