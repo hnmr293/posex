d240aba234adfba1b435e9aec26ce3f7a1200244
Jacob <jacob.zhik@gmail.com>
2024-02-03 23:29:10 +0300

    fix load script to webui

--- javascript/posex-webui.js
+++ javascript/posex-webui.js
@@ -1,115 +1,67 @@
 (function () {
     if (!globalThis.posex) globalThis.posex = {};
     const posex = globalThis.posex;
+    let posix_executed = false;
+    let isLoadScript = false;
+    const loadJsScripts = async (base_path) => {
+        globalThis.posex.import = async () => {
+            const THREE = await import(`${base_path}/three.module.js`);
+            const { TrackballControls } = await import(`${base_path}/TrackballControls.js`);
+            const { DragControls } = await import(`${base_path}/DragControls.js`);
+            const { MeshLine, MeshLineMaterial } = await import(`${base_path}/THREE.MeshLine.Module.min.js`);
+            return { THREE, TrackballControls, DragControls, MeshLine, MeshLineMaterial };
+        };
+        if (!globalThis.posex.imports) globalThis.posex.imports = {};
+        if (!globalThis.posex.imports.three) globalThis.posex.imports.three = async () => await import(`${base_path}/three.module.js`);
+        if (!globalThis.posex.imports.posex) globalThis.posex.imports.posex = async () => await import(`${base_path}/posex.js`);
 
-    function load(cont) {
-        function load_() {
-            if (posex.script_loading || posex.script_loaded) return;
-            posex.script_loading = true;
+    }
 
-            const scripts = cont.textContent.trim().split('\n');
-            const base_path = `/file=${scripts.shift()}/js`;
-            cont.textContent = '';
-
-            const df = document.createDocumentFragment();
-            for (let src of scripts) {
-                console.log(`[Posex] loading ${src}`);
-                const script = document.createElement('script');
-                script.async = true;
-                script.type = 'module';
-                script.src = `file=${src}`;
-                df.appendChild(script);
-            }
-
-            globalThis.posex.import = async () => {
-                const THREE = await import(`${base_path}/three.module.js`);
-                const { TrackballControls } = await import(`${base_path}/TrackballControls.js`);
-                const { DragControls } = await import(`${base_path}/DragControls.js`);
-                const { MeshLine, MeshLineMaterial } = await import(`${base_path}/THREE.MeshLine.Module.min.js`);
-                return { THREE, TrackballControls, DragControls, MeshLine, MeshLineMaterial };
-            };
-            if (!globalThis.posex.imports) globalThis.posex.imports = {};
-            if (!globalThis.posex.imports.three) globalThis.posex.imports.three = async () => await import(`${base_path}/three.module.js`);
-            if (!globalThis.posex.imports.posex) globalThis.posex.imports.posex = async () => await import(`${base_path}/posex.js`);
-            cont.appendChild(df);
+    async function load(type) {
+        const acc = gradioApp().querySelector(`#posex-${type}-accordion`);
+        if (!acc) return;
+        //load scripts contents
+        const scriptsContainer = acc.querySelector(`#posex-${type}-js #posex-${type}-js`);
+        if (!scriptsContainer) return;
+        const scripts = scriptsContainer.textContent.trim().split('\n');
+        scriptsContainer.textContent = '';
+        const chgeckBoxInit = acc.querySelector(`#posex-${type}-enabled input[type=checkbox]`);
+        posex.script_loading = true;
+        const base_path = `/file=${scripts.shift()}/js`;
+        const df = document.createDocumentFragment();
+        for (let src of scripts) {
+            console.log(`[Posex] loading ${src}`);
+            const script = document.createElement('script');
+            script.async = true;
+            script.type = 'module';
+            script.src = `file=${src}`;
+            df.appendChild(script);
         }
+        if (!isLoadScript) {
+            loadJsScripts(base_path)
+            isLoadScript = true;
+        }
+        scriptsContainer.appendChild(df);
 
-        return posex.script_loaded ? Promise.resolve() : new Promise(resolve => {
-            document.addEventListener('posexscriptloaded', () => resolve(), false);
-            load_();
-        });
+        document.addEventListener('posexscriptloaded', () => {
+            chgeckBoxInit.addEventListener('change', async () => {
+                await posex[`init_${type}`]();
+                console.log(`[Posex] ${type} initialized`);
+            }, { once: true });
+        }, false);
     }
 
-    function lazy(fn, timeout) {
-        if (timeout === undefined) timeout = 500;
-        return new Promise(function callback(resolve) {
-            const result = fn();
-            if (result) {
-                resolve(result);
-            } else {
-                setTimeout(() => callback(resolve), timeout);
-            }
-        });
-    }
 
-    // function hook_acc(acc, fn) {
-    //     const observer = new MutationObserver(list => {
-    //         for (let mut of list) {
-    //             if (mut.type === 'childList') {
-    //                 if (mut.addedNodes.length != 0) {
-    //                     // closed -> opened
-    //                     fn();
-    //                 } else {
-    //                     // opened -> closed
-    //                     // do nothing
-    //                 }
-    //             }
-    //         }
-    //     });
-    //     observer.observe(acc, { childList: true, attributes: false, subtree: false });
-    // }
     window.addEventListener('DOMContentLoaded', () => {
-        const observer = new MutationObserver((m) => {
-            const types = ['t2i', 'i2i']
-            types.forEach(async (type) => {
-                if (gradioApp().querySelector(`#posex-${type}-accordion`) && gradioApp()?.querySelector(`#posex-${type}-accordion`)) {
-                    const acc = gradioApp()?.querySelector(`#posex-${type}-accordion`);
-                    const cont = Array.from(acc.querySelectorAll(`#posex-${type}-js`)).at(-1);
-                    const enabled = acc.querySelector(`#posex-${type}-enabled input[type=checkbox]`);
-                    await load(cont);
-                    if (enabled.checked) {
-                        await posex[`init_${type}`]();
-                        console.log(`[Posex] ${type} initialized`);
-                    } else {
-                        enabled.addEventListener('change', async () => {
-                            await posex[`init_${type}`]();
-                            console.log(`[Posex] ${type} initialized`);
-                        }, { once: true });
-                    }
-                    observer.disconnect();
-                }
-            })
+        const observer = new MutationObserver(async (m) => {
+            if (!posix_executed && gradioApp().querySelector(`#posex-t2i-accordion`)) {
+                posix_executed = true;
+                load('t2i');
+                load('i2i')
+                observer.disconnect();
+            }
 
         })
-        observer.observe(depth_gradioApp(), { childList: true, subtree: true })
+        observer.observe(gradioApp(), { childList: true, subtree: true })
     })
-    // function launch(type) {
-    //     return lazy(() => gradioApp()?.querySelector(`#posex-${type}-accordion`)).
-    //         then(acc => hook_acc(acc, async () => {
-    //             const cont = Array.from(acc.querySelectorAll(`#posex-${type}-js`)).at(-1); // !
-    //             const enabled = acc.querySelector(`#posex-${type}-enabled input[type=checkbox]`);
-    //             await load(cont);
-    //             if (enabled.checked) {
-    //                 await posex[`init_${type}`]();
-    //                 console.log(`[Posex] ${type} initialized`);
-    //             } else {
-    //                 enabled.addEventListener('change', async () => {
-    //                     await posex[`init_${type}`]();
-    //                     console.log(`[Posex] ${type} initialized`);
-    //                 }, { once: true });
-    //             }
-    //         }));
-    // }
-    // launch('t2i');
-    // launch('i2i');
 })();
